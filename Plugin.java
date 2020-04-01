6f09e6958c3b835c6e318d6569ea17239eb3ed84
Smeety <44217655+Smeety@users.noreply.github.com>
2020-04-01 21:50:44 +0200

    Update Plugin.java

--- Plugin.java
+++ Plugin.java
@@ -1,56 +1,16 @@
-/*
- * Copyright (c) 2016-2017, Adam <Adam@sigterm.info>
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- * 1. Redistributions of source code must retain the above copyright notice, this
- *    list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright notice,
- *    this list of conditions and the following disclaimer in the documentation
- *    and/or other materials provided with the distribution.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
- * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-package net.runelite.client.plugins;
-
-import com.google.inject.Binder;
-import com.google.inject.Injector;
-import com.google.inject.Module;
-
-public abstract class Plugin implements Module
-{
-	protected Injector injector;
-
-	@Override
-	public void configure(Binder binder)
-	{
-	}
-
-	protected void startUp() throws Exception
-	{
-	}
-
-	protected void shutDown() throws Exception
-	{
-	}
-
-	public void resetConfiguration()
-	{
-	}
-
-	public final Injector getInjector()
-	{
-		return injector;
-	}
-}
+  
+[
+  {
+    "projectUrl": "test.com",
+    "provider": "Smeety",
+    "name": "Zulrah",
+    "description": "What more is there to say?",
+    "id": "zulrah-plugin",
+    "releases": [
+      {
+        "date": "2020-02-24",
+        "sha512sum": "022BE40B8D45A47264D92A90183027B8322095E5BDF2E3D0E733F5A33D7EDFFF067984A64EC619601B3D87ED42FB9ED0E6B43E8EFD25F264F584DB4A26950C2A",
+        "version": "5.9.0",
+        "url": "https://github.com/Smeety/OldPlugins/tree/master/zulrah",
+        "requires": "0.0.1"
+      },
